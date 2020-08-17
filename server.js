2f774f71f958c735784629728842828300ab679d
Danielle Maria Ali <dani.maria.ali@gmail.com>
2020-08-17 07:38:54 -0400

    Now properly using the database for logging in and out
    Switched from previous users array to connecting to the database for logging in and out

--- server.js
+++ server.js
@@ -9,17 +9,13 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 // use the environment port (for heroku deployment) or 3000 if used locally
 const PORT = process.env.PORT || 3000
-//for encrypting the password
-const bcrypt = require ('bcrypt')
-// handle authentication   
-const passport = require ('passport') 
+
+
 // show flash messages
 const flash = require ('express-flash')
 const session = require ('express-session')
 //allows us to use methods other than POST and GET in forms
 const methodOverride = require ('method-override')
-//use to generate random uuid for room ids
-const {v4: uuidV4} = require ('uuid')
 // make it easier to access the different input elements from our server
 const bodyParser = require('body-parser')
 
@@ -37,16 +33,7 @@ db.once('open', () => console.error("Connected to Mongoose"))
 const userRouter = require("./routes/users")
 const indexRouter = require("./routes/index")
 
-//NOT IDEAL - need to connect to database
-const users = []
 const User = require('./models/user')
-
-const initializePassport = require ('./passport_config.js')
-initializePassport(
-  passport, 
-  username => users.find(user => user.username === username), 
-  id => users.find(user => user.id === id)
-)
 
 //set up how we will access our views - using ejs which we installed
 app.set('view engine', 'ejs')
@@ -67,8 +54,7 @@ app.use(session ({
   resave: false,
   saveUninitialized : false
 }))
-app.use(passport.initialize())
-app.use(passport.session())
+
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({limit:'10mb', extended: false}))
 
@@ -77,61 +63,6 @@ app.use(bodyParser.urlencoded({limit:'10mb', extended: false}))
 // got to UserRouter for anything with subdomain /users
 app.use('/users', userRouter)
 app.use('/', indexRouter)
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login.ejs')
-})
-app.get('/register', checkNotAuthenticated, (req, res) => {
-  res.render('register.ejs')
-})
-// dynamically assign the roomid to the uuid that's generated
-app.get('/:room', (req, res) => {
-  res.render('room.ejs', {roomId: req.params.room})
-})
-
-// if the user is not authenticated, take them to the login page
-function checkAuthenticated (req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-  res.redirect('/login')
-}
-
-app.post ('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect : `/${uuidV4()}`,
-  failureRedirect: '/login', 
-  failureFlash: true
-}))
-
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-        //10 is the cost factor ie 2 to the power 10 expansion rounds
-        const hashedPassword = await bcrypt.hash (req.body.password, 10) 
-        users.push({
-          id: Date.now().toString(),
-          username: req.body.username, 
-          password: hashedPassword
-        })
-        res.redirect('/login')
-    }
-    catch {
-        res.redirect('/register')
-    }
-})
-
-// add logout functionality
-app.delete('/logout', (req, res)=> {
-  req.logOut()
-  res.redirect('/login')
-})
-
-// if user already authenticated don't take them to the register or login pages
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/')
-  }
-  next()
-}
 
 //------------ CONFIGURING SOCKET.IO ----------------
 
